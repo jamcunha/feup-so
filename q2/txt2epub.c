21228e91352a8c23b43c7c6d52be80a86319cf7c
Joaquim Cunha <joaquimafonsocunha@gmail.com>
2022-11-17 03:51:59 +0000

    adicionada makefile para q2

---
+++ q2/txt2epub.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[]) {
+    /* check if arguments are present */
+    if(argc < 2) {
+        printf("usage: txt2epub file [file]\n");
+        return EXIT_FAILURE;
+    }
+
+    /* se tiver so um ficheiro nao fazer fork mas fazer logo o comando */
+    
+    return EXIT_SUCCESS;
+}
+

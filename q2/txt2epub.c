9fd1d3e0d8fbe6eea28efdbc535b6fb993e5b311
Joaquim Cunha <joaquimafonsocunha@gmail.com>
2022-11-18 17:50:46 +0000

    Added epub creation (needs error handling)

--- q2/txt2epub.c
+++ q2/txt2epub.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int main (int argc, char *argv[]) {
     /* check if arguments are present */
@@ -8,7 +12,39 @@ int main (int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    /* se tiver so um ficheiro nao fazer fork mas fazer logo o comando */
+    pid_t pids[argc-1];
+    for(int i = 1; i < argc; i++) {
+        if((pids[i-1] = fork()) == -1) {
+            perror("fork");
+            return EXIT_FAILURE;
+        } else if(pids[i-1] == 0) {
+            printf("[pid%d] converting %s ...\n", getpid(), argv[i]);
+
+            char *epub_ext = ".epub";
+            char *epub = (char*)malloc(strlen(argv[i])+1);
+            strcpy(epub, argv[i]);
+            epub[strlen(epub)-4] = '\0';
+            strcat(epub, epub_ext);
+
+            /* adding pandoc args to an array to use execvp */
+            char **exec = (char**)malloc(4 * sizeof(char*));
+            exec[0] = "pandoc";
+            exec[1] = argv[i];
+            exec[2] = "-o";
+            exec[3] = epub;
+            exec[4] = NULL;
+
+            execvp(exec[0], exec);
+        }
+    }
+
+    /* wait for all child processes to end */
+    for(int i = 0; i < argc-1; i++) {
+        if(waitpid(pids[i], NULL, 0) == -1) {
+            perror("wait");
+            return EXIT_FAILURE;
+        }
+    }
     
     return EXIT_SUCCESS;
 }

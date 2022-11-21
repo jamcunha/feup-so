db4c09acd2b67cc0d1a68a16a27c842ab6ddb886
Joaquim Cunha <joaquimafonsocunha@gmail.com>
2022-11-21 00:06:02 +0000

    q2 done

--- q2/txt2epub.c
+++ q2/txt2epub.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <errno.h>
 
 int main (int argc, char *argv[]) {
     /* check if arguments are present */
@@ -14,9 +15,9 @@ int main (int argc, char *argv[]) {
 
     pid_t pids[argc-1];
     for(int i = 1; i < argc; i++) {
-        if((pids[i-1] = fork()) == -1) {
-            perror("fork");
-            return EXIT_FAILURE;
+        if((pids[i-1] = fork()) < 0) {
+            fprintf(stderr, "%s: fork error: %s\n", argv[0], strerror(errno));
+            exit(EXIT_FAILURE);
         } else if(pids[i-1] == 0) {
             printf("[pid%d] converting %s ...\n", getpid(), argv[i]);
 
@@ -27,7 +28,7 @@ int main (int argc, char *argv[]) {
             strcat(epub, epub_ext);
 
             /* adding pandoc args to an array to use execvp */
-            char **exec = (char**)malloc(4 * sizeof(char*));
+            char **exec = (char**)malloc(5 * sizeof(char*));
             exec[0] = "pandoc";
             exec[1] = argv[i];
             exec[2] = "-o";
@@ -35,13 +36,35 @@ int main (int argc, char *argv[]) {
             exec[4] = NULL;
 
             execvp(exec[0], exec);
+
+            /* execvp error */
+            fprintf(stderr, "%s: couldn't convert file to epub: %s\n", argv[0], strerror(errno));
+            exit(EXIT_FAILURE);
         }
     }
 
     /* wait for all child processes to end */
     for(int i = 0; i < argc-1; i++) {
-        if(waitpid(pids[i], NULL, 0) == -1) {
-            perror("wait");
+        if(waitpid(pids[i], NULL, 0) < 0) {
+            fprintf(stderr, "%s: waitpid error: %s\n", argv[0], strerror(errno));
+            return EXIT_FAILURE;
+        }
+    }
+
+    /* zip epubs */
+    pid_t pid;
+    if((pid = fork()) < 0) {
+        fprintf(stderr, "%s: fork error: %s\n", argv[0], strerror(errno));
+        exit(EXIT_FAILURE);
+    } else if(pid == 0) {
+        system("zip ebooks.zip *.epub");
+        if(system("zip ebooks.zip *.epub") < 0) {
+            fprintf(stderr, "%s: couldn't compress epub files: %s\n", argv[0], strerror(errno));
+            return EXIT_FAILURE;
+        }
+    } else {
+        if(waitpid(pid, NULL, 0) < 0) {
+            fprintf(stderr, "%s: waitpid error: %s\n", argv[0], strerror(errno));
             return EXIT_FAILURE;
         }
     }
